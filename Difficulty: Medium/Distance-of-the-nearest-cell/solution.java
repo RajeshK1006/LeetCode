9df15da4d6e32fc8c03303b86cc23beb3fc68461
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-01-27 12:00:49 +0530

    Update solution.java

--- Difficulty: Medium/Distance-of-the-nearest-cell/solution.java
+++ Difficulty: Medium/Distance-of-the-nearest-cell/solution.java
@@ -1,40 +1,33 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-// Position this line where user code will be pasted.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
             int[][] grid = new int[n][m];
-
-            for (int i = 0; i < n; i++) {
-
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = sc.nextInt();
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    grid[i][j] = Integer.parseInt(S[j]);
                 }
             }
-            int[] source = new int[2];
-            for (int i = 0; i < 2; i++) {
-                int x = sc.nextInt();
-                source[i] = x;
+            Solution obj = new Solution();
+            int[][] ans = obj.nearest(grid);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+                System.out.println();
             }
-            int[] dest = new int[2];
-            for (int i = 0; i < 2; i++) {
-                int x = sc.nextInt();
-                dest[i] = x;
-            }
-            Solution ob = new Solution();
-            int ans = ob.shortestPath(grid, source, dest);
-            System.out.println(ans);
         
 System.out.println("~");
 }
@@ -43,56 +36,47 @@ System.out.println("~");
 // } Driver Code Ends
 
 
-// User function Template for Java
+class Solution
+{
+    //Function to find distance of nearest 1 in the grid for each cell.
+    public int[][] nearest(int[][] grid)
+    {
+        // Code here
+        Queue<int[]> q = new LinkedList<>();
+        int r = grid.length;
+        int c = grid[0].length;
 
-class Solution {
-
-    int shortestPath(int[][] grid, int[] source, int[] destination) {
-
-        // Your code here
-        int N = grid.length;
-        int M = grid[0].length;
-        int src_R = source[0];
-        int src_C = source[1];
-        int des_R = destination[0];
-        int des_C = destination[1];
-        
-        if(grid[src_R][src_C] ==0 || grid[des_R][des_C] == 0){
-            return -1;
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)-> x[0] - y[0]);
-        pq.offer(new int[] {src_R,src_C,0});
-        int[][] visit = new int[N][M];
-        visit[src_R][src_C] = 1;
-        
-        int[][] dir = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
-        
-        while (!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int dis = curr[0];
-            int rr = curr[1];
-            int cc = curr[2];
-            
-            if(rr == des_R & cc == des_C){
-                return dis;
+        int[][] h = new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    h[i][j]=0;
+                    q.offer(new int[] {i,j});
+                }
+                else{
+                    h[i][j] = -1;
+                }
             }
-            
+        }
+        int[][] dir = new int[][] {{0,1},{0,-1} ,{1,0},{-1,0}};
         
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int rr = curr[0];
+            int cc = curr[1];
             
             for(int i=0;i<4;i++){
                 int nx = rr + dir[i][0];
                 int ny = cc + dir[i][1];
                 
-                if(nx<0 || nx>=N|| ny<0 || ny>=M || grid[nx][ny] != 1 ||visit[nx][ny] != 0 ) continue;
-                visit[nx][ny] = 1;
-                int newdistance = dis + 1;
-            
-                pq.offer(new int[] {newdistance ,nx,ny});
+                if(nx<0 || nx >= r || ny <0 || ny >= c|| h[nx][ny]!= -1) continue;
                 
+                h[nx][ny] = 1 + h[rr][cc];
+                q.offer(new int[] {nx,ny});
             }
         }
         
-        return -1;
+        return h;
+        
     }
 }
