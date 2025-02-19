bea1c6c5484ee846160748d99035b3cfcae7b4e5
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-02-19 07:24:53 +0530

    Time: 17 ms (4.31%) | Memory: 58.2 MB (5.31%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,18 +6,14 @@ class Solution {
             pre[i] = pre[i-1] * nums[i-1];
         }
         System.out.println(Arrays.toString(pre));
-        int[] sff = new int[nums.length];
-        sff[nums.length-1] = 1;
-        for(int i = nums.length-2;i>=0;i--){
-
-            sff[i] = sff[i+1] * nums[i+1];
+        int sff = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            pre[i] *= sff;
+            sff *= nums[i];
         }
-        System.out.println(Arrays.toString(sff));
 
-        for(int i=0;i<nums.length;i++){
+        return pre;
+        
 
-            nums[i] = pre[i] * sff[i];
-        }
-        return nums;
-     }
+    } 
 }
