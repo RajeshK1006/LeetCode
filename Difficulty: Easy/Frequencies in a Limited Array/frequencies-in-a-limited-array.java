314558a5edc6e5feffe459c071df90195d40548a
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-04-13 23:01:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().frequencyCount(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count the frequency of all elements from 1 to N in the array.
+    public List<Integer> frequencyCount(int[] arr) {
+        // do modify in the given array
+        int n = arr.length;
+        int[] result = new int[n];
+
+        for (int val : arr) {
+            if (val >= 1 && val <= n) {
+                result[val - 1]++;
+            }
+        }
+
+        List<Integer> a  = new ArrayList<>();
+        for(int num : result){
+            a.add(num);
+        }
+        
+        return a;
+        
+    }
+}
